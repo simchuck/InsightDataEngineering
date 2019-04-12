13a627ed0dc786a6e77214abee918ec14d032d85
simchuck <charles.simchick@gmail.com>
2019-04-12 05:15:09 -0700

    Add optional delay parameter to test_kafka_topic.py

--- src/data-ingestion/src/test_kafka_topic.py
+++ src/data-ingestion/src/test_kafka_topic.py
@@ -9,8 +9,35 @@ Reads from specified S3 bucket and writes each line to Kafka topic.
 
 import argparse
 #import logging
+from time import sleep
 
 from kafka import KafkaProducer,KafkaConsumer
+
+#import chunk_it
+def chunk_it(iterable, size):
+    """
+    Parse the iterable into chunks of the specified size.
+
+    Input:
+        iterable    iter    any iterable data structure
+        size        int     number of elements to return
+
+    Yields:
+        'size' elements of the iterable
+    """
+    count = 0
+    chunk = []
+    for item in iterable:
+        if count < size:
+            count += 1
+            chunk.append(item)
+        else:
+            yield(chunk)
+            count = 0
+            chunk = []
+    if len(chunk) > 0:
+        yield(chunk)
+
 
 #logger = logging.basicConfig(level=logging.DEBUG)
 
@@ -35,7 +62,7 @@ for k,v in S.items():
     S[k]['url'] = '/'.join([s3_bucket, S[k]['folder'], S[k]['file']])
 
 
-def produce_record(source, brokers, topic):
+def produce_record(source, brokers, topic, delay=0):
     """
     Read each line from specified source, small transformation, and publish to topic.
     """
@@ -56,6 +83,7 @@ def produce_record(source, brokers, topic):
 #        fields = line.strip().encode('utf-8').split(',')
 #        record = ','.join(fields[0], fields[2])
         producer.send(topic, fields)
+        sleep(1.0 * int(delay) / 1000.)
 #        producer.flush()       ### WHAT DOES THIS DO?
 
 
@@ -76,7 +104,7 @@ def consume_record(brokers, topic):
 #        yield (record.timestamp, record.value)
 
 
-def simulate_streams_for_resource_node(topic):
+def simulate_streams_for_resource_node(topic, delay=0):
 
     for stream in STREAM_INFO:
     #for (topic, s3_directory, s3_filename) in STREAM_INFO:
@@ -90,8 +118,8 @@ def simulate_streams_for_resource_node(topic):
         #    '\tbrokers:\t{2}\n'.format(s3_source, topic, brokers))
 
         try:
-            produce_record(stream['url'], brokers, topic)
-            #produce_record(s3_source, brokers, topic)
+            produce_record(stream['url'], brokers, topic, delay)
+            #produce_record(s3_source, brokers, topic, delay)
         except Exception as e:
             print('ERROR: Could not publish to topic {0}'.format(topic))
             raise e
@@ -99,14 +127,14 @@ def simulate_streams_for_resource_node(topic):
     return
 
 
-def simulate_topic_stream(topic, source_data):
+def simulate_topic_stream(topic, source_data, delay=0):
 
     print('\nStarting Kafka producer test with\n' \
         '\tsource: \t{0}\n' \
         '\ttopic:  \t{1}\n'.format(source_data, topic))
 
     try:
-        produce_record(source_data, brokers, topic)
+        produce_record(source_data, brokers, topic, delay)
     except Exception as e:
         print('ERROR: Could not publish to topic {0}'.format(topic))
         raise e
@@ -150,6 +178,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Test Producer/Consumer for Kafka cluster.')
     parser.add_argument('-m', '--mode', help='Specify producer/consumer mode')
     parser.add_argument('-t', '--topic', help='Specify Kafka topic')
+    parser.add_argument('-d', '--delay', default=0, help='Delay (ms) between successive records')
     args = parser.parse_args()
 
     if args.mode == 'producer':
@@ -159,8 +188,10 @@ if __name__ == '__main__':
             s3_url = s3_bucket + '/energy-price/pjm_rt_fivemin_hrl_lmps.csv'
         elif args.topic == 'demand':
             s3_url = s3_bucket + '/mis-data/mains.dat'
-        simulate_topic_stream(args.topic, s3_url)
-        #simulate_streams_for_resource_node(topic)
+
+        simulate_topic_stream(args.topic, s3_url, args.delay)
+        #simulate_streams_for_resource_node(args.topic, args.delay)
+
     elif args.mode == 'consumer':
         record = consume_topic_stream(args.topic)
         #consume_streams_for_resource_node()
